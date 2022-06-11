626e283264051a0f88eb2c8b6a854fd973f176a9
Arthur0Rocha <ar_d_rocha@hotmail.com>
2022-06-11 11:16:28 -0300

    Channel selection and routing working

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ import os
 # Event data CC -> [0]: channel, [4]: param, [5]: value
 # Event data Note -> [0]: channel, [1]: note, [2]: velocity, [3]: off_velocity, [4]: duration
 # In a client without queue, received events have no time information and are asigned dummy queue number 253
+DUMMY_QUEUE_NUMBER = 253
+DUMMY_FLAG = 1
 
 # SYSTEM EVENTS 0 AND 1
 # SND_SEQ_EVENT_SYSEX = 130
 # CLIENT, PORT EVENTS FROM 60 TO 67
 
-NOTES = {'C-1': 0, 'C0': 12, 'A0': 21, 'B3': 59} # TODO create this (until G9?)
+BASE_NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+NOTES_DICT = {'C-1': 0, 'C0': 12, 'A0': 21, 'B3': 59} # TODO create this (until G9?)
+NOTES_LIST = []
 
 HIGH = 127
 LOW = 0
@@ -54,6 +58,8 @@ CC_F8_KUSER4 = 21
 
 # 74 71 79 72 | 17 19 20 21 
 
+MAX_CHANNEL = 16
+
 status = {
     'layers_channels': [],
     'layers_offset': [],
@@ -61,26 +67,33 @@ status = {
     'split_note': 0,
     'notes_hanging': [],
     'mode': '',
+    'SW_active': []
 }
 
 def panic():
     for chan in range(16):
-        pass # send PANIC (123) event
-    return # TODO implement this
+        alsaseq.output(create_CC_event(chan, CC_PANIC, 0))
 
 def reset_status():
     panic()
     status['layers_channels'] = [0]
     status['layers_offset'] = [0]
     status['layers_active'] = 1
-    status['split_note'] = NOTES['B3']
+    status['split_note'] = NOTES_DICT['B3']
     status['mode'] = 'play'
+    status['SW_active'] = [0, 0]
 
-def create_CC_event():
-    return
+def create_CC_event(channel, param, value):
+    return (CC_CODE, DUMMY_FLAG, 0, DUMMY_QUEUE_NUMBER, (0, 0), (0, 0), (0, 0), (channel, 0, 0, 0, param, value))
 
-def send_ev():
-    event = ()
+def send_ev(ev):
+    """
+        Performs channel routing
+    """
+    head = ev[:7]
+    data = ev[7][1:]
+    channel = status['layers_channels'][0]
+    event = head + (((channel,) + data),)
     alsaseq.output(event)
 
 def check_connections_fail():
@@ -94,8 +107,15 @@ def connect_devices():
 #       >>> alsaseq.connectto( 1, 130, 0 )
 
 def init():
-    reset_status()
+    for octave in range(-1, 12):
+        for ind, note in enumerate(BASE_NOTES):
+            index = ind + 12 * (octave + 1)
+            note += str(octave)
+            NOTES_LIST.append(note)
+            NOTES_DICT[note] = index
+            
     alsaseq.client('FSynth-Controller', 1, 1, False)
+    reset_status()
 
 def loop():
     while True:
@@ -108,44 +128,41 @@ def loop():
 
             evtype = ev[0]
             data = ev[7]
-            channel = data[0]
-
+            
             if evtype == CC_CODE:
                 param = data[4]
                 value = data[5]
                 if param == CC_FOOT_SW and value == HIGH:
-                    print(f"CC: FOOT SW - {param} {value} - Channel {channel}")
-                    pass # CC 82 (Foot SW) HIGH
+                    status['layers_channels'][0] = (status['layers_channels'][0] + 1) % MAX_CHANNEL
+                elif param == CC_SW1:
+                    status['SW_active'][0] = 1 if value == HIGH else 0
+                elif param == CC_SW2:
+                    status['SW_active'][1] = 1 if value == HIGH else 0
                 elif param >= CC_USER_1 and param <= CC_USER_4 and value == HIGH:
-                    print(f"CC: USER - {param} {value} - Channel {channel}")
-                    pass # CC 26 <=> 29 HIGH
-                elif param == CC_PEDAL:
-                    print(f"CC: Pedal - {param} {value} - Channel {channel}")
-                    pass # CC 04 (Pedal)
+                    chan = param - CC_USER_1 # CC_USER_1 TO CC_USER_4 ARE SEQUENTIAL IN VALUE
+                    sw1, sw2 = status['SW_active']
+                    status['layers_channels'][0] = chan + 4*sw1 + 8*sw2
+                # elif param == CC_PEDAL:
+                #     pass
+                elif param == CC_PANIC:
+                    alsaseq.output(ev) # Direct output disconsidering channel routing
                 else:
-                    print(f"CC: {param} {value} - Channel {channel}")
+                    send_ev(ev)
 
             elif evtype == AT_CODE:
-                param = data[4]
-                value = data[5]
-                print(f"AT: {param} {value} - Channel {channel}")
-                pass # AT
+                send_ev(ev)
 
             elif evtype == PGM_CHANGE_CODE:
-                param = data[4]
-                value = data[5]
-                print(f"PGM CHNG: {param} {value} - Channel {channel}")
-                pass
+                send_ev(ev)
 
             elif evtype == NOTEON_CODE:
-                note, velocity, off_velocity, duration = data[1:]
-                print(f"NOTE ON: {note} {velocity} {off_velocity} {duration} - Channel {channel}")
-                pass
+                send_ev(ev)
 
             elif evtype == NOTEOFF_CODE:
-                note, velocity, off_velocity, duration = data[1:]
-                print(f"NOTE OFF: {note} {velocity} {off_velocity} {duration} - Channel {channel}")
-                pass
+                send_ev(ev)
+            
+            else:
+                send_ev(ev)
 
 
 
